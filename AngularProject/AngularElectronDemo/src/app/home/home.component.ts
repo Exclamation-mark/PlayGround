3ac99dc8a38d851f2ec4493b1a193acaa56ed394
BreakZhao <1694392889@qq.com>
2021-11-24 15:57:03 +0800

    electron render try

--- AngularProject/AngularElectronDemo/src/app/home/home.component.ts
+++ AngularProject/AngularElectronDemo/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {ElectronService} from "../core/services";
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,15 @@ export class HomeComponent implements OnInit {
 
   editorOptions = {theme: 'vs-light', language: 'javascript'};
   code = 'function x() {\nconsole.log("Hello world!");\n}';
-  constructor(private router: Router) { }
+
+  constructor(private router: Router, private electronService: ElectronService) {
+  }
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
   }
 
+  reload() {
+    this.electronService.reload();
+  }
 }
