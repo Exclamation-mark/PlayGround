e70fe17180ce5876410098c470593c318aeb98a4
BreakZhao <1694392889@qq.com>
2021-12-01 11:11:07 +0800

    add custom component

---
+++ AngularProject/AntDesignTheme/src/app/pages/welcome/button-cell-renderer/button-cell-renderer.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {AgRendererComponent} from 'ag-grid-angular';
+import {IAfterGuiAttachedParams, ICellRendererParams} from 'ag-grid-community';
+import { NzMessageService } from 'ng-zorro-antd/message';
+
+@Component({
+  selector: 'app-button-cell-renderer',
+  templateUrl: './button-cell-renderer.component.html',
+  styleUrls: ['./button-cell-renderer.component.less']
+})
+export class ButtonCellRendererComponent implements OnInit, AgRendererComponent {
+  dataItem: any;
+  constructor(private message: NzMessageService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  afterGuiAttached(params?: IAfterGuiAttachedParams): void {
+  }
+
+  agInit(params: ICellRendererParams): void {
+    this.dataItem = params.data;
+    console.log('zzq see', this.dataItem);
+  }
+
+  refresh(params: ICellRendererParams): boolean {
+    return false;
+  }
+
+  run() {
+    this.message.success('run ' + this.dataItem.id);
+  }
+
+  diff() {
+    this.message.success('diff ' + this.dataItem.id);
+  }
+
+  detail() {
+    this.message.success('detail ' + this.dataItem.id);
+  }
+}
