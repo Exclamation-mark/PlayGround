1287f3f0d9db49d7c02fbd3fcf04e7f7e815f67b
BreakZhao <1694392889@qq.com>
2021-12-17 16:07:07 +0800

    add Table Demo

--- JavaProject/WebSocketDemo/src/test/java/com/example/TableDemoTest.java
+++ JavaProject/WebSocketDemo/src/test/java/com/example/TableDemoTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.sarojaba.prettytable4j.PrettyTable;
 import io.bretty.console.table.Alignment;
 import io.bretty.console.table.Table;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,17 @@ public class TableDemoTest {
         };
         Table table = Table.of(data, Alignment.CENTER, 10); // 10-character wide for each column
         System.out.println(table); // NOTICE: table.toString() is called implicitly
+    }
+
+    @Test
+    public void testString2() {
+        PrettyTable pt = PrettyTable
+                .fieldNames("name", "age", "city")
+                .addRow("johnassssssssssssssssssssssssssssssssssss", 22, "new york")
+                .addRow("elizabeth", 426165223, "chsaddddddddddddddddddddddddddasdddddddddddddddddddddddddddddddddddddddddicago")
+                .addRow("bill", 31, "atxsasdasdasdddddddddddddddddddddddddddddddddddddddddlanta")
+                .addRow("mary", 18, "los angeles");
+
+        System.out.println(pt);
     }
 }
