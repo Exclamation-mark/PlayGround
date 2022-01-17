6cc84ae026170d52f3283e863f69dfe55782ea23
BreakZhao <1694392889@qq.com>
2022-01-17 13:59:57 +0800

    add test

--- JavaProject/WebSocketDemo/src/main/java/com/example/webscoketdemo/beans/Result.java
+++ JavaProject/WebSocketDemo/src/main/java/com/example/webscoketdemo/beans/Result.java
@@ -3,39 +3,46 @@ package com.example.webscoketdemo.beans;
 import com.example.webscoketdemo.WebScoketDemoApplication;
 import com.example.webscoketdemo.controller.AbstractJsonBody;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.NullNode;
 
 /**
  * @author zzq
  */
 public class Result<T> extends AbstractJsonBody {
+    private static final String DATA = "data";
+    private static final String MSG = "msg";
+    private static final String RESULT = "result";
+    private static final String SUCCESS_TIP = "success";
 
     public static <T> Result<T> success(T data) {
         Result<T> result = new Result<>();
-        result.set("data", WebScoketDemoApplication.MAPPER.valueToTree(data));
-        result.put("msg", "success");
-        result.put("result", true);
+        result.set(DATA, WebScoketDemoApplication.MAPPER.valueToTree(data));
+        result.put(MSG, SUCCESS_TIP);
+        result.put(RESULT, true);
         return result;
     }
 
     public static Result<String> successWithStr(String data) {
         Result<String> result = new Result<>();
-        result.put("msg", data);
-        result.put("result", true);
+        result.set(DATA, NullNode.getInstance());
+        result.put(MSG, data);
+        result.put(RESULT, true);
         return result;
     }
 
     public static Result<String> successWithStrData(String data) {
         Result<String> result = new Result<>();
-        result.put("msg", "success");
-        result.put("data", data);
-        result.put("result", true);
+        result.put(MSG, SUCCESS_TIP);
+        result.put(DATA, data);
+        result.put(RESULT, true);
         return result;
     }
 
     public static Result<String> error(String errorMsg) {
         Result<String> result = new Result<>();
-        result.put("msg", errorMsg);
-        result.put("result", true);
+        result.put(MSG, errorMsg);
+        result.set(DATA, NullNode.getInstance());
+        result.put(RESULT, true);
         return result;
     }
 }
--- JavaProject/WebSocketDemo/src/test/java/com/example/webscoketdemo/beans/ResultTest.java
+++ JavaProject/WebSocketDemo/src/test/java/com/example/webscoketdemo/beans/ResultTest.java
@@ -2,6 +2,7 @@ package com.example.webscoketdemo.beans;
 
 import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,17 +21,36 @@ class ResultTest {
         Cat sd = new Cat.CatBuilder().name("sd").yearsOld(15).build();
         Result<Cat> catResult = Result.success(sd);
         System.out.println(catResult.toPrettyString());
+        assertTrue(StringUtils.contains(catResult.toPrettyString(), "\"msg\" : \"success\""));
+        assertTrue(StringUtils.contains(catResult.toPrettyString(), "\"result\" : true"));
+        assertTrue(StringUtils.contains(catResult.toPrettyString(), "\"yearsOld\" : 15"));
+        assertTrue(StringUtils.contains(catResult.toPrettyString(), "\"name\" : \"sd\""));
     }
 
     @Test
     void successWithStr() {
+        Result<String> result = Result.successWithStr("test");
+        System.out.println(result.toPrettyString());
+        assertTrue(StringUtils.contains(result.toPrettyString(), "\"msg\" : \"test\""));
+        assertTrue(StringUtils.contains(result.toPrettyString(), "\"data\" : null"));
+        assertTrue(StringUtils.contains(result.toPrettyString(), "\"result\" : true"));
     }
 
     @Test
     void successWithStrData() {
+        Result<String> result = Result.successWithStrData("test");
+        System.out.println(result.toPrettyString());
+        assertTrue(StringUtils.contains(result.toPrettyString(), "\"data\" : \"test\""));
+        assertTrue(StringUtils.contains(result.toPrettyString(), "\"msg\" : \"success\""));
+        assertTrue(StringUtils.contains(result.toPrettyString(), "\"result\" : true"));
     }
 
     @Test
     void error() {
+        Result<String> error = Result.error("test");
+        System.out.println(error.toPrettyString());
+        assertTrue(StringUtils.contains(error.toPrettyString(), "\"data\" : null"));
+        assertTrue(StringUtils.contains(error.toPrettyString(), "\"msg\" : \"test\""));
+        assertTrue(StringUtils.contains(error.toPrettyString(), "\"result\" : true"));
     }
 }
