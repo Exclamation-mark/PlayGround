23243f8727a37f6ef70e88fafdfe8c2edf164413
BreakZhao <1694392889@qq.com>
2022-01-21 14:39:22 +0800

    test mvl2

--- JavaProject/WebSocketDemo/src/test/java/com/example/MvelTest.java
+++ JavaProject/WebSocketDemo/src/test/java/com/example/MvelTest.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import com.deliveredtechnologies.rulebook.FactMap;
+import com.deliveredtechnologies.rulebook.lang.RuleBookBuilder;
+import com.deliveredtechnologies.rulebook.model.RuleBook;
 import com.google.common.collect.Maps;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,10 +19,31 @@ public class MvelTest {
         Map<String, Object> map = Maps.newHashMap();
         map.put("a", null);
         map.put("b", null);
+        map.put("c", 10);
 
         Object object = MVEL.eval(expression, map);
         Assertions.assertTrue(object != null);
         Assertions.assertTrue(object instanceof Boolean);
         Assertions.assertTrue(object.equals(true));
+
+        Object greR = MVEL.eval("c >= 10", map);
+        Assertions.assertTrue(greR != null);
+        Assertions.assertTrue(greR instanceof Boolean);
+        Assertions.assertTrue(greR.equals(true));
+
+        Object greR2 = MVEL.eval("c * 2 > 20", map);
+        Assertions.assertTrue(greR2 != null);
+        Assertions.assertTrue(greR2 instanceof Boolean);
+        Assertions.assertTrue(greR2.equals(false));
+    }
+
+    @Test
+    public void testOgnl() {
+        RuleBook ruleBook = RuleBookBuilder.create()
+                .addRule(rule -> rule.withNoSpecifiedFactType()
+                        .then(f -> System.out.print("Hello "))
+                        .then(f -> System.out.println("World")))
+                .build();
+        ruleBook.run(new FactMap());
     }
 }
