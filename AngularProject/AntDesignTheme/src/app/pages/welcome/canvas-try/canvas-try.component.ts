fa907e5d91464cfb5e68d7dd1f78d2c03129205d
BreakZhao <1694392889@qq.com>
2022-07-26 17:02:16 +0800

    draw grid

--- AngularProject/AntDesignTheme/src/app/pages/welcome/canvas-try/canvas-try.component.ts
+++ AngularProject/AntDesignTheme/src/app/pages/welcome/canvas-try/canvas-try.component.ts
@@ -20,32 +20,69 @@ export class CanvasTryComponent implements OnInit, AfterViewInit {
     const ctx = this.myCanvas.nativeElement.getContext('2d');
     if (ctx !== null) {
       this.context = ctx;
-      const grid = {
-        width: 0,
-        height: 0,
+
+      const DEFAULT_OPTIONS = {
+        cols       : 10,
+        rows       : 10,
+        width      : 100,
+        height     : 100,
+        weight     : 1,
+        background : null,
+        color      : '#000000'
       };
 
-      grid.width = 25;
-      grid.height = 25;
-      const canvasWidth = 500;
-      const canvasHeight = 500;
+      const addGrid = (options: any) => {
+        createCanvasGrid(options);
+      };
 
-      this.myCanvas.nativeElement.style.width = canvasWidth + 'px';
-      this.myCanvas.nativeElement.style.height = canvasWidth + 'px';
-      this.myCanvas.nativeElement.style.border = '1px solid black';
-      ctx.beginPath();
-      ctx.lineWidth = 1;
-      // @ts-ignore
-      ctx.lineStyle = 'black';
-      for (let x = 0; x < canvasWidth; x += canvasWidth / grid.width) {
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, canvasHeight);
-      }
-      for (let y = 0; y < canvasHeight; y += canvasHeight / grid.height) {
-        ctx.moveTo(0, y);
-        ctx.lineTo(canvasWidth, y);
-      }
-      ctx.stroke();
+      const createCanvasGrid = (options: any) => {
+        const opts = Object.assign({}, DEFAULT_OPTIONS, options);
+        console.log('zzq see v', opts);
+        const canvas = this.myCanvas;
+        this.myCanvas.nativeElement.width = opts.width;
+        this.myCanvas.nativeElement.height = opts.height;
+
+        const weight2 = opts.weight * 2;
+        const weightHalf = opts.weight / 2;
+
+        const availWidth =  opts.width - opts.weight;
+        const availHeight = opts.height - opts.weight;
+
+        const cellWidth = availWidth / opts.cols;
+        const cellHeight = availHeight / opts.rows;
+
+        if (options.background) {
+          ctx.fillStyle = opts.background;
+          ctx.fillRect(0, 0, opts.width, opts.height);
+        }
+
+        ctx.beginPath();
+        ctx.strokeStyle = opts.color;
+        ctx.lineWidth = opts.weight;
+
+        for (let col = 0; col <= opts.cols; col++) {
+          const newX = Math.floor(col * cellWidth) + weightHalf;
+          ctx.moveTo(newX, 0);
+          ctx.lineTo(newX, opts.height);
+        }
+
+        for (let row = 0; row <= opts.rows; row++) {
+          const newY = (row * cellHeight) + weightHalf;
+          ctx.moveTo(0, newY);
+          ctx.lineTo(opts.width, newY);
+        }
+
+        ctx.stroke();
+
+        return canvas;
+      };
+      addGrid({
+        width: 500,
+        height: 500,
+        background: '#FFF',
+        color: '#999'
+      });
+
     }
   }
 }
