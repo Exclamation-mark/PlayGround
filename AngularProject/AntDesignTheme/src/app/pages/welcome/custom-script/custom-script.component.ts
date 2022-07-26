3aee08f85c7d427b96cc8cd893bd9a70870853ae
BreakZhao <1694392889@qq.com>
2022-07-26 14:43:54 +0800

    set editor

--- AngularProject/AntDesignTheme/src/app/pages/welcome/custom-script/custom-script.component.ts
+++ AngularProject/AntDesignTheme/src/app/pages/welcome/custom-script/custom-script.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomScriptComponent implements OnInit {
   isRunning = false;
+  editorOptions = {theme: 'vs-light', language: 'javascript'};
+  code = `
+function controlRobot(robot, holder){
+
+}
+  `;
   constructor() { }
 
   ngOnInit(): void {
