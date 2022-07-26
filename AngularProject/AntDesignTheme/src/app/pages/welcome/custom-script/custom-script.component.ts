ade691a1e9cac785fc0bb42124577e22ef8df30f
BreakZhao <1694392889@qq.com>
2022-07-26 14:26:01 +0800

    add custom js script route

---
+++ AngularProject/AntDesignTheme/src/app/pages/welcome/custom-script/custom-script.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-script',
+  templateUrl: './custom-script.component.html',
+  styleUrls: ['./custom-script.component.less']
+})
+export class CustomScriptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
