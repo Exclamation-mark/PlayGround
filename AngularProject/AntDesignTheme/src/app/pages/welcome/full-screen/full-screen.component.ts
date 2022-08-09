46fee5f2300ca8a89a977b6d5d3135635da92290
BreakZhao <1694392889@qq.com>
2022-08-09 11:30:11 +0800

    add content

--- AngularProject/AntDesignTheme/src/app/pages/welcome/full-screen/full-screen.component.ts
+++ AngularProject/AntDesignTheme/src/app/pages/welcome/full-screen/full-screen.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./full-screen.component.less']
 })
 export class FullScreenComponent implements OnInit {
-
+  editorOptions = {theme: 'vs-light', language: 'json'};
+  code = '';
   constructor() { }
 
   ngOnInit(): void {
+    const a = [];
+    for (let i = 0; i < 1000; i++) {
+      a.push({
+        index: i,
+        time: new Date().getTime(),
+        name: new Date().toDateString()
+      });
+    }
+    this.code = JSON.stringify(a, null, '\t');
   }
 
 }
